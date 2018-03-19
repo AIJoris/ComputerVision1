c5bb3282d27a9ada9491d5975a55e5f53258c643
Joris <jsbaan@gmail.com>
2018-03-19 18:02:09 +0100

    added filenames for test set

--- part1/main.m
+++ part1/main.m
@@ -5,11 +5,11 @@
 %% Specify paramters
 n = 10;             % number of images to load
 r = 0.5;           % ratio of train set to use for vocabbuilding/training 
-method = 'opsift'; % feature extraction method
+method = 'sift'; % feature extraction method
 k = 400;            % number of words in visual vocabulary
 
 %% Load images in cell structures, where each cell contains 4 data sets
-[train1, train2, test] = load_data(n,r);
+[train1, train2, test, test_names] = load_data(n,r);
 
 %% Extract SIFT features from images in train set
 disp('Extracting features...');
