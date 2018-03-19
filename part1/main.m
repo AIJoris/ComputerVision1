6b0bae088d558e68554f6638c9729d1472dce1fb
Joris <jsbaan@gmail.com>
2018-03-19 20:23:02 +0100

    added some html parameters

--- part1/main.m
+++ part1/main.m
@@ -1,12 +1,10 @@
-%% TODO
-% Classify using SVD
-% Compute metircs (AP etc)
-
 %% Specify paramters
-n = 10;             % number of images to load
-r = 0.5;           % ratio of train set to use for vocabbuilding/training 
-method = 'sift'; % feature extraction method
+n = 10;             % number of images to load (n<400)
+r = 0.5;            % ratio of train set to use for vocabbuilding/training 
+method = 'sift';    % feature extraction method
 k = 400;            % number of words in visual vocabulary
+n_pos = n-ceil(n * r);
+n_neg = 3 * n_pos;
 
 %% Load images in cell structures, where each cell contains 4 data sets
 [train1, train2, test, test_names] = load_data(n,r);
@@ -28,3 +26,14 @@ hist_test = sift2hist(sift_test, C);
 %% Train SVM on second half of train set to classify images
 % model = train(training_label_vector, training_instance_matrix [,'liblinear_options', 'col']);
 % [predicted_label, accuracy, decision_values/prob_estimates] = predict(testing_label_vector, testing_instance_matrix, model [, 'liblinear_options', 'col']);
+
+%% Write parameters to handle HTML parsing in Python
+%AP
+%MAP
+%names
+
+
+
+
+
+
