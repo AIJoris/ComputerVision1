a6643118bfcee48865363e2f4913f4ca8c22f27b
Joris <jsbaan@gmail.com>
2018-03-19 14:13:17 +0100

    pijn en ellende

--- part1/sift2hist.m
+++ part1/sift2hist.m
@@ -1,11 +1,11 @@
 %% Represent images using visual vocabulary
 function [train_hist_cell] = sift2hist(train_feature_cell,C)
 train_hist_cell = cell(size(train_feature_cell));
-
 % For each image, for each feature, find closest 'word' in vocab
 for i = 1:length(train_feature_cell)
     dataset_feature_cell = train_feature_cell{i};
-    features = zeros(length(dataset_feature_cell),length(C));
+    features = zeros(length(dataset_feature_cell), size(C,1));
+
     for j = 1:length(dataset_feature_cell)
         im_features = double(dataset_feature_cell{j});
         % Compute the distances between the vocabulary words and the
